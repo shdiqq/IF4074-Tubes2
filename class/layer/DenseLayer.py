aca9ee11a515c81d79497cf189e564f6878cd90e
Shadiq Harwiz <13520038@std.stei.itb.ac.id>
2023-09-27 14:45:43 +0700

    fix backward on the dense layer

--- class/layer/DenseLayer.py
+++ class/layer/DenseLayer.py
@@ -17,8 +17,10 @@ class DenseLayer():
     self.bias = np.zeros((numUnit,))
     self.deltaBias = None
     self.output = None
+    self.input = None
 
   def forward(self, inputData):
+    self.input = inputData
     numFeatures = np.prod(inputData.shape)
     if (self.weight is None) :
       self.weight = np.random.randn(numFeatures, self.numUnit)
@@ -36,23 +38,37 @@ class DenseLayer():
     self.output = output
     return output
   
-  def backward(self, inputData, prevLayerDelta):
+  def backward(self, dError_dOutput):
+    # print(f"dE/dO = {dError_dOutput}")
     if (self.activationFunctionName.lower() == 'relu'):
-      dOutput_dInput = relu(self.output, derivative=True)
+      dOutput_dNet = relu(self.output, derivative=True)
     elif (self.activationFunctionName.lower() == 'sigmoid'):
-      dOutput_dInput = sigmoid(self.output, derivative=True)
+      dOutput_dNet = sigmoid(self.output, derivative=True)
     elif (self.activationFunctionName.lower() == 'linear'):
-      dOutput_dInput = linear(self.output, derivative=True)
+      dOutput_dNet = linear(self.output, derivative=True)
     # elif (self.activationFunctionName.lower() == 'softmax'):
     #   derivativeValues = softmax(self.output, derivative=True)
-    dError_dInput = prevLayerDelta * dOutput_dInput
-    self.deltaWeight = np.outer(inputData, dError_dInput)
-    dError_dInput = np.dot(dError_dInput, self.weight.T)
+    # print(f"dO/dNet = {dOutput_dNet}")
+    # print(f"dNet/dW = {self.input}")
+    deltaError = dError_dOutput * dOutput_dNet
+    # print(f"dE/dNet = {deltaError}")
+    for i in range (len(self.deltaWeight)):
+      self.deltaWeight[i] = self.input[i] * deltaError
+    # print(f"dE/dW = {self.deltaWeight}")
+    self.deltaBias = deltaError
+    # print(f"dE/dBias = {self.deltaBias}")
+    dError_dOutputBeforeLayer = np.dot(deltaError, self.weight.T)
+    # print(f"dE/dOutputBeforeLayer = {dError_dOutputBeforeLayer}")
+    return dError_dOutputBeforeLayer
 
-    # Menghitung gradien bias
-    self.deltaBias = dError_dInput
+  def resetDelta(self):
+    self.deltaWeight = None
 
-    return dError_dInput
+  def updateWeightBias(self, learningRate, momentum):
+    for i in range (len(self.weight)):
+      self.weight[i][0] = self.weight[0] - ( (momentum * self.weight[i][0]) + (learningRate * self.deltaWeight[i][0] * self.input[i]) )
+    self.bias[0] = self.bias[0] - ( (momentum * self.bias[0]) + (learningRate * self.deltaBias[0]) )
+    self.resetDelta()
 
 ### TESTING ###
 if __name__ == "__main__":
